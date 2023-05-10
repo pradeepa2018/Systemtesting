94c8c15bb09482a12d704be040b1041cc9ef1d54
pradeepa2018 <132225294+pradeepa2018@users.noreply.github.com>
2023-05-10 09:03:06 +0530

    day02
    Datatype, class, package, main

---
+++ src/day02/program3.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class program3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Pradeepa..!");
+		System.out.println("Good Morning..!");
+		System.out.println("I Need To Create a Pacakge");
+		
+	}
+
+}
